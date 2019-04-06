672dbae5add61169942800345cb1fe13ced7aaa6
pknn1 <pakanon.pk@gmail.com>
2019-04-07 00:37:57 +0700

    Minor change

--- components/JobCard.vue
+++ components/JobCard.vue
@@ -1,19 +1,18 @@
 <template>
   <nuxt-link :to="{ path: `/jobs/${job.id - 1}` }">
     <el-card class="box-card">
-      <div slot="header" class="clearfix">
-        <span>Title :</span>
-        <el-button style="float: right; padding: 3px 0" type="text">{{
-          job.title
-        }}</el-button>
-      </div>
-      <div>
-        Status: {{ job.status }}
-        <br />
-        Due date: {{ job.due }}
-        <br />
-        Company: {{ job.company }}
-      </div>
+      <h4>{{ job.title }}</h4>
+      <p class="status">
+        <i
+          :class="
+            job.status === 'Approved' ? 'el-icon-success' : 'el-icon-error'
+          "
+        />
+        {{ job.status }}
+        <span class="dim">
+          <i class="el-icon-time" /> Due in {{ job.due }}
+        </span>
+      </p>
     </el-card>
   </nuxt-link>
 </template>
@@ -30,8 +29,25 @@ export default {
 }
 </script>
 
-<style>
-.box-card {
-  flex-basis: 33.3%;
+<style lang="scss" scoped>
+.flex-grid > a {
+  flex-basis: 50%;
+  padding: 1em 1em;
+}
+
+.el-card {
+  min-height: 50px;
+}
+
+.el-icon-success {
+  color: rgb(103, 194, 58);
+}
+
+.el-icon-error {
+  color: #f56c6c;
+}
+
+.status {
+  margin-top: 1em;
 }
 </style>
