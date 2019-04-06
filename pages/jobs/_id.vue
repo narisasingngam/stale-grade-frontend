b9356a6a1ab0e4daeed7ad8b6f77e673cf4ba757
narisasingngam <mmintttt@gmail.com>
2019-04-06 22:42:43 +0700

    add description

---
+++ pages/jobs/_id.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="container">
+    <div class="head-content">
+      <h1>{{ job.title }}</h1>
+    </div>
+    <br />
+    <br />
+    Status: {{ job.status }}
+    <br />
+    Due date: {{ job.due }}
+    <br />
+    Company: {{ job.company }}
+    <div><br /><br />{{ job.description }}</div>
+    <NuxtChild />
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      id: Number(this.$route.params.id)
+    }
+  },
+  computed: {
+    job() {
+      return this.$store.state.jobs[this.id]
+    }
+    // isStudent() {
+    //   return this.$store.state.currentUser.role === 'student'
+    // }
+  },
+  methods: {
+    back() {
+      this.$route.back()
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  padding: 6em 12em;
+}
+
+.head-content {
+  display: flex;
+  align-items: center;
+  h1 {
+    margin-right: 3em;
+  }
+}
+</style>
